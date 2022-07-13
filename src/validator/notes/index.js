a22ac22535763c702d2097da58b7ecc44b819ce9
Fityan <fityannugroho@gmail.com>
2022-07-13 13:40:29 +0700

    Create validation files
    - Create `validator/notes/schema.js` to store `NotePayloadSchema`.
    - Create `validator/notes/index.js` to store `NotesValidator`.

---
+++ src/validator/notes/index.js
@@ -0,0 +1,12 @@
+const { NotePayloadSchema } = require('./schema');
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw validationResult.error;
+    }
+  },
+};
+
+module.exports = NotesValidator;
