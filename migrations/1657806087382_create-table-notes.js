e9f00794a48faf0b4f23d9852db8203d1d35b7c5
Fityan <fityannugroho@gmail.com>
2022-07-14 21:20:22 +0700

    Create `notes` table migration

---
+++ migrations/1657806087382_create-table-notes.js
@@ -0,0 +1,36 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('notes', {
+    id: {
+      type: 'VARCHAR(16)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(100)',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    tags: {
+      type: 'TEXT[]',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TIMESTAMPTZ',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'TIMESTAMPTZ',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('notes');
+};
