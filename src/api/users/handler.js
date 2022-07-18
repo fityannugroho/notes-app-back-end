c87fce10336547d5fa269157f0c3c2792c237b13
Fityan <fityannugroho@gmail.com>
2022-07-18 20:56:16 +0700

    Create and register users plugin

---
+++ src/api/users/handler.js
@@ -0,0 +1,65 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    // Bind handler methods to this class.
+    this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+  }
+
+  async postUserHandler(request, h) {
+    try {
+      this._validator.validateUserPayload(request.payload);
+
+      const { username, password, fullname } = request.payload;
+      const userId = await this._service.addUser({ username, password, fullname });
+
+      return h.response({
+        status: 'success',
+        message: 'User berhasil ditambahkan',
+        data: { userId },
+      }).code(201);
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return h.response({
+          status: 'fail',
+          message: error.message,
+        }).code(error.statusCode);
+      }
+
+      return h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      }).code(500);
+    }
+  }
+
+  async getUserByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const user = await this._service.getUserById(id);
+
+      return h.response({
+        status: 'success',
+        data: { user },
+      });
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return h.response({
+          status: 'fail',
+          message: error.message,
+        }).code(error.statusCode);
+      }
+
+      return h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      }).code(500);
+    }
+  }
+}
+
+module.exports = UsersHandler;
--- src/server.js
+++ src/server.js
@@ -4,9 +4,11 @@ const Hapi = require('@hapi/hapi');
 const NotesService = require('./services/postgres/NotesService');
 const notes = require('./api/notes');
 const NotesValidator = require('./validator/notes');
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
-  const notesService = new NotesService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,14 +19,25 @@ const init = async () => {
     },
   });
 
-  // Register the notes plugin.
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  // Register the plugins.
+  await server.register([
+    // Notes plugin
+    {
+      plugin: notes,
+      options: {
+        service: new NotesService(),
+        validator: NotesValidator,
+      },
     },
-  });
+    // Users plugin.
+    {
+      plugin: users,
+      options: {
+        service: new UsersService(),
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
