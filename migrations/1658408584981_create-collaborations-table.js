af9b72d318a2f756257b315926a2716823a241fd
Fityan <fityannugroho@gmail.com>
2022-07-21 20:05:45 +0700

    Create `collaborations` migration table

---
+++ migrations/1658408584981_create-collaborations-table.js
@@ -0,0 +1,20 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('collaborations', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    note_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('collaborations');
+};
