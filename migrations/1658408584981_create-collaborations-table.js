bdab109df24592acaf85b359fa56003e7ab94c3c
Fityan <fityannugroho@gmail.com>
2022-07-21 20:15:48 +0700

    Add `user_id`, unique and FK constraints

--- migrations/1658408584981_create-collaborations-table.js
+++ migrations/1658408584981_create-collaborations-table.js
@@ -12,7 +12,21 @@ exports.up = (pgm) => {
       type: 'VARCHAR(50)',
       notNull: true,
     },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
   });
+
+  /*
+    Add a unique constraint, combining the note_id and user_id columns.
+    This will prevent two users from collaborating on the same note.
+   */
+  pgm.addConstraint('collaborations', 'unique_note_id_and_user_id', 'UNIQUE (note_id, user_id)');
+
+  // Add a foreign key constraint on the note_id and user_id columns.
+  pgm.addConstraint('collaborations', 'fk_collaborations.note_id_notes.id', 'FOREIGN KEY (note_id) REFERENCES notes (id) ON DELETE CASCADE');
+  pgm.addConstraint('collaborations', 'fk_collaborations.user_id_users.id', 'FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
